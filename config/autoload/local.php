bc3c623b22138607c5a5328e3b8164123152e7f6
farleir <farleir@farleir>
2013-01-25 14:36:28 -0200

    Last Version

---
+++ config/autoload/local.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Local Configuration Override
+ *
+ * This configuration override file is for overriding environment-specific and
+ * security-sensitive configuration information. Copy this file without the
+ * .dist extension at the end and populate values as needed.
+ *
+ * @NOTE: This file is ignored from Git by default with the .gitignore included
+ * in ZendSkeletonApplication. This is a good practice, as it prevents sensitive
+ * credentials from accidentally being committed into version control.
+ */
+
+return array(
+    'db' => array(
+        'username' => 'root',
+        'password' => '123456',
+    ),
+    
+// Doctrine configuration (added 03/12/12) NOT WORKING YET------------------
+    'doctrine' => array(
+        'connection' => array(
+            'orm_default' => array(
+                'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
+                'params' => array(
+                    'host'     => 'localhost',
+                    'port'     => '3306',
+                    'user'     => 'root',
+                    'password' => '123456',
+                    'dbname'   => 'portaldecursos',
+                )
+            )
+        )
+    ),
+);
