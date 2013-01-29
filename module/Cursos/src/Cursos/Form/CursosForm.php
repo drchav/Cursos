0da8f9f3d45e08441c3c9f76246eff67e34cd44d
guilherme <guilherme@guilhermebattisti>
2013-01-29 15:25:03 -0200

    Adicionado campos novos

--- module/Cursos/src/Cursos/Form/CursosForm.php
+++ module/Cursos/src/Cursos/Form/CursosForm.php
@@ -22,7 +22,7 @@ class CursosForm extends Form
                 'type'  => 'text',
             ),
             'options' => array(
-                'label' => 'Nome do Curso',
+                 'label' => 'Nome do Curso: ',
             ),
         ));
         $this->add(array(
@@ -31,7 +31,70 @@ class CursosForm extends Form
                 'type'  => 'textarea',
             ),
             'options' => array(
-                'label' => 'Descrição do Curso',
+                'label' => 'Descrição do Curso: ',
+            ),
+        ));
+          $this->add(array(
+            'name' => 'turno',
+            'attributes' => array(
+                'type'  => 'text',
+            ),
+            'options' => array(
+                'label' => 'Turno: ',
+            ),
+        ));
+            $this->add(array(
+            'name' => 'vagas',
+            'attributes' => array(
+                'type'  => 'text',
+            ),
+            'options' => array(
+                'label' => 'Vagas: ',
+            ),
+        ));
+             $this->add(array(
+            'name' => 'carga_horaria',
+            'attributes' => array(
+                'type'  => 'text',
+            ),
+            'options' => array(
+                'label' => 'Carga Horária: ',
+            ),
+        ));
+              $this->add(array(
+            'name' => 'duracao_semestral',
+            'attributes' => array(
+                'type'  => 'text',
+            ),
+            'options' => array(
+                'label' => 'Semestres: ',
+            ),
+        ));
+                 $this->add(array(
+            'name' => 'coordenador',
+            'attributes' => array(
+                'type'  => 'text',
+            ),
+            'options' => array(
+                'label' => 'Coordenador: ',
+            ),
+        ));
+                    $this->add(array(
+            'name' => 'email',
+            'attributes' => array(
+                'type'  => 'text',
+            ),
+            'options' => array(
+                'label' => 'E-Mail: ',
+            ),
+        ));
+                       $this->add(array(
+            'name' => 'telefone',
+            'attributes' => array(
+                'type'  => 'text',
+            ),
+            'options' => array(
+                'label' => 'Telefone: ',
             ),
         ));
         $this->add(array(
