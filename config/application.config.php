600d86032aca5927aa2f4808f74cbc220fb6ec57
farleir <farleir@farleir>
2013-01-30 22:04:39 -0200

    Adicionado novo módulo de autenticação ZfUser

--- config/application.config.php
+++ config/application.config.php
@@ -5,8 +5,8 @@ return array(
         //'DoctrineModule', 
         //'DoctrineORMModule', 
 	'Cursos',
-        //'ZfcBase',
-        //'ZfcUser',
+        'ZfcBase',
+        'ZfcUser',
     ),
     'module_listener_options' => array(
         'config_glob_paths'    => array(
